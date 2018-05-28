f0f22160ae31e68b1fe263dfcff3bcbde473bb32
kvin1087 <kaustubh87@gmail.com>
2018-05-28 09:13:36 -0700

    Added basic node.js

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 require('./instanthello');
 var goodbye = require('./talk/goodbye');
-goodbye();
+var talk = require('./talk');
+var question =  require('./talk/question');
+
+talk.intro();
+talk.hello('kvin');
+
+var answer = question.ask('What is the meaning of life?');
+console.log(answer);
